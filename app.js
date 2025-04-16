98ab81eeeaf02b6812a2828bc494a673ed5ba54f
SilverPro17 <rodriguessilvano242@gmail.com>
2025-04-16 11:20:51 -0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const bookRoutes = require('./Routes/bookRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./Docs/swagger');
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/books', bookRoutes);
 
 const PORT = 3000;
